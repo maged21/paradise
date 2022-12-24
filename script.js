9a12e15d0bc3acb2dfe4d7e1d00f287453de586e
maged21 <majedabdulaziz72@yahoo.com>
2022-12-24 15:46:55 +0200

    updates

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-// const nav = document.querySelector("#header");
-
-// window.addEventListener ("scroll", function() {
-//   nav.classList.toggle ("sticky", window.scrollY > 0);
-// }) ;
-
-var sidemenu = document.getElementById("sidemenu");
-
-function openmenu(){
-  sidemenu.style.right = "0";
-}
-function closemenu(){
-  sidemenu.style.right = "-200px";
-}
-
 function toggleMenu() {
   const toggleMenu = document.querySelector(".toggleMenu");
   const navigation = document.querySelector(".navigation");
   toggleMenu.classList.toggle("active");
   navigation.classList.toggle("active");
 }
+
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+  breakpoints: {
+    0:{
+        slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    }
+  },
+});
