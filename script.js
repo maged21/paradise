20d5be811737db8b432956e1a12b08b50fdcfea5
maged21 <majedabdulaziz72@yahoo.com>
2023-01-10 16:57:21 +0200

    last update

--- script.js
+++ script.js
@@ -46,3 +46,10 @@ var swiper = new Swiper(".slide-content", {
     }
   },
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+  header.classList.toggle ("sticky", window.scrollY > 0);
+}) ;
+
